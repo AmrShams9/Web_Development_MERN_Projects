38631905d631e2c239b75815bd3ef8afc74d2cc7
Amr Shams <102551102+AmrShams9@users.noreply.github.com>
2024-06-18 22:39:33 +0300

    Quiz App

---
+++ Quiz_App/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from '../src/assets/Components/Header.jsx'
+import Quiz from './assets/Components/Quiz.jsx';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </>
+    );
+}
+
+export default App;
---
+++ Quiz_App/src/assets/Components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+
+    return <header>
+        <img src="quiz-logo.png" />
+        <h1>Quiz App</h1>
+    </header>
+
+
+}
